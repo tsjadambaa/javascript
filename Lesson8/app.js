e5597b3ee8e5711e1de04222e080acf502adc805
unknown <jadambaa@jadambaa.ecustoms.mn>
2020-03-20 09:13:19 +0800

    lesson9 commit

---
+++ Lesson8/app.js
@@ -0,0 +1,8 @@
+var a =12;
+a="11"+a+true+true+true+true+false+'2';
+console.log(a);
+var result=4>2>=1;
+var result=4+2*5;
+console.log(4+2*5);
+console.log((4+2)*5);
+console.log((4)*5);
---
+++ Lesson9/app.js
@@ -0,0 +1,1 @@
+alert("sain baina uu");
